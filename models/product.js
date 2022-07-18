9bd84b3a5abbb5a119da8a9ce4167745c475eb3d
jennymylee <jennylee038@gmail.com>
2022-07-18 14:21:46 -0700

    can search for a product w query

--- models/product.js
+++ models/product.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 const { BadRequestError, NotFoundError } = require("../utils/errors");
 const axios = require("axios");
 const fetch = require("node-fetch");
 const StockXAPI = require("stockx-api");
 const stockX = new StockXAPI();
+
 // import fetch from "node-fetch";
 
 // const { get } = require("../app");
 
 class Product {
-  static async getProducts() {
+  static async getAllProducts() {
     const options = {
       url: "https://the-sneaker-database.p.rapidapi.com/sneakers",
       method: "GET",
@@ -31,6 +33,27 @@ class Product {
     try {
       response = await axios.request(options);
       //   console.log("getProducts response:", response);
+      return response.data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async searchProducts(q) {
+    const options = {
+      method: "GET",
+      url: "https://the-sneaker-database.p.rapidapi.com/search",
+      params: { limit: 10, query: q.query },
+      headers: {
+        "X-RapidAPI-Key": "b644290787mshfee7bb22060308dp13940bjsnf0ed4c49b267",
+        "X-RapidAPI-Host": "the-sneaker-database.p.rapidapi.com",
+      },
+    };
+
+    let response = null;
+    try {
+      response = await axios.request(options);
+      console.log("searchProducts response:", response);
       return response.data.results;
     } catch (err) {
       console.log(err);
--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,18 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const products = await Product.getProducts();
+    const products = await Product.getAllProducts();
+    // console.log("products in routes", products);
+    return res.status(200).json({ products });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/search", async (req, res, next) => {
+  try {
+    // req.body is an object in the format {"query": "Jordan"}
+    const products = await Product.searchProducts(req.body);
     // console.log("products in routes", products);
     return res.status(200).json({ products });
   } catch (err) {

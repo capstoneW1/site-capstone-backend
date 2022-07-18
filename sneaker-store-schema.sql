14204ab53b66fe628294c802370388d3a542d45e
jennymylee <jennylee038@gmail.com>
2022-07-18 15:22:19 -0700

    updated schema

--- sneaker-store-schema.sql
+++ sneaker-store-schema.sql
@@ -8,21 +8,10 @@ CREATE TABLE users (
     shoe_size   INTEGER
 );
 
-CREATE TABLE shoe (
-    id              SERIAL PRIMARY KEY,
-    name            TEXT NOT NULL,
-    brand           TEXT NOT NULL,
-    release_date    TEXT NOT NULL,
-    description     TEXT NOT NULL,
-    price           NUMERIC(5, 2) NOT NULL,
-    img_url         TEXT NOT NULL
-);
-
 CREATE TABLE wishlist (
     id          SERIAL PRIMARY KEY,
     user_id     INTEGER NOT NULL,
     shoe_id     INTEGER NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (shoe_id) REFERENCES shoe(id) ON DELETE CASCADE
 );

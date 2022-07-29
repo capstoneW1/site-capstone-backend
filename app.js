e7af4b937cb5b13819d0b4ddf618b896c5b203d7
jennymylee <jennylee038@gmail.com>
2022-07-29 12:09:39 -0700

    made email routes

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 const security = require("./middleware/security.js");
 const { NotFoundError } = require("./utils/errors");
 
-const authRoutes = require("./routes/auth")
+const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product.js");
 const wishlistRoutes = require("./routes/wishlist");
+const emailRouter = require("./routes/nodeMailer");
 
 const app = express();
 
@@ -21,12 +22,21 @@ app.use(function (req, res, next) {
 
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cors());
+
+// this variable allows us to make requests with nodemailer
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(security.extractUserFromJwt);
 
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
 app.use("/product", productRoutes);
 app.use("/wishlist", wishlistRoutes);
+app.use("/nodemailer", emailRouter);
 
 app.get("/", async (req, res, next) => {
   res.status(200).json({ ping: "pong" });
---
+++ routes/nodeMailer.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const nodemailer = require("nodemailer");
+
+const emailRouter = express();
+
+emailRouter.use(express.json());
+
+emailRouter.post("/send", (req, res) => {
+  // route to send an email
+  // takes in an email address and a text body
+  const { email, messageHtml } = req.body;
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    service: "gmail",
+    auth: {
+      user: "workday1codepath@gmail.com",
+      pass: "bteu gxod dqyz crjx",
+    },
+
+    from: "workday1codepath@gmail.com",
+  });
+
+  const mail = {
+    from: "Sneaker Store workday1codepath@gmail.com",
+    to: email,
+    subject: "You've Signed Up to Receive Alerts",
+    html: messageHtml,
+  };
+
+  transporter.sendMail(mail, (err) => {
+    if (err) {
+      res.status(400).send("Fail");
+    } else {
+      res.status(200).send("Success");
+    }
+  });
+});
+
+module.exports = emailRouter;

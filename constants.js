61e20954c9f97a7f1aff233517162dec84b0277a
dylancocoletzi <86747062+dylancocoletzi@users.noreply.github.com>
2022-08-04 14:05:30 -0700

    deploying express api

---
+++ constants.js
@@ -0,0 +1,7 @@
+// const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "http://elevate-sneakers.herokuapp.com/";
+// export default API_BASE_URL;
+
+module.exports = {
+    API_BASE_URL
+}
